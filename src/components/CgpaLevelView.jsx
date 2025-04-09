27e38b47f59ba3f19f01bb84502a4f822f31b7e8
liltexh <liltexhnology@gmail.com>
2025-04-09 20:15:20 +0100

    changed padding for calculator

--- src/components/CgpaLevelView.jsx
+++ src/components/CgpaLevelView.jsx
@@ -41,14 +41,16 @@ export default function CgpaLevelView({ result, viewingResult }) {
 	}, [gpaResults]);
 	return (
 		<section className="fixed inset-0 backdrop-blur-2xl flex justify-center items-center h-dvh z-50">
-			<div className="flex flex-col justify-evenly bg-gray-50 w-[80%]  rounded-xl p-4 z-0 border-2 border-gray-500 gap-6">
+			<div className="flex flex-col justify-evenly bg-gray-50 w-[80%]  rounded-xl p-4 z-0 border-2 border-gray-500 gap-4">
 				<div className="flex flex-col justify-center gap-4">
 					<h3 className="text-3xl font-semibold">{result.level}</h3>
-					<div className="bg-gray-500 px-4 py-1 rounded-md flex justify-between text-gray-50">
-						<button className="bg-gray-300 px-8 py-1 rounded-md shadow-xl">
+					<div className="bg-gray-500 px-1 md:px-4 py-1 rounded-md flex justify-between text-gray-50 gap-2">
+						<button className="bg-gray-300 py-1 rounded-md shadow-xl whitespace-nowrap flex-1">
 							First Semester
 						</button>
-						<button>Second Semester</button>
+						<button className="py-1 rounded-md whitespace-nowrap flex-1">
+							Second Semester
+						</button>
 					</div>
 				</div>
 				<div className="flex flex-col justify-center items-center gap-2">
@@ -79,14 +81,14 @@ export default function CgpaLevelView({ result, viewingResult }) {
 						})}
 					</div>
 				</div>
-				<div className="flex flex-col w-full">
-					<div className="flex border justify-evenly gap-4 rounded-sm p-2">
+				<div className="flex flex-col w-full gap-4">
+					<div className="flex justify-evenly gap-4 rounded-sm">
 						<input
 							type="text"
 							name="course"
 							value={gpaRes.course}
 							placeholder="Course"
-							className="shrink min-w-10 flex-1 inset-shadow-md"
+							className="shrink min-w-10 flex-1 border-2 border-gray-500 rounded-md py-1 px-2 shadow-xl"
 							onChange={editRes}
 						/>
 						<input
@@ -94,7 +96,7 @@ export default function CgpaLevelView({ result, viewingResult }) {
 							placeholder="Unit"
 							name="unit"
 							value={gpaRes.unit}
-							className="shrink min-w-10 flex-1"
+							className="shrink min-w-10 flex-1 border-2 border-gray-500 rounded-md py-1 px-2 shadow-xl"
 							onChange={editRes}
 						/>
 						<input
@@ -102,15 +104,17 @@ export default function CgpaLevelView({ result, viewingResult }) {
 							placeholder="Grade"
 							name="grade"
 							value={gpaRes.grade}
-							className="shrink min-w-10 flex-1"
+							className="shrink min-w-10 flex-1 border-2 border-gray-500 rounded-md py-1 px-2 shadow-xl"
 							onChange={editRes}
 						/>
 					</div>
-					<div>
-						<Button01
-							texts="Add"
-							doclick={addResult}
-						/>
+					<div className="flex justify-center items-center">
+						<button
+							className="bg-gray-500 w-full text-gray-50 py-2 rounded-md shadow-md"
+							onClick={addResult}
+						>
+							Add
+						</button>
 					</div>
 				</div>
 			</div>
